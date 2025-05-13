3b57210431a95a03f6e632ffae50b5853469b82d
Miro <miropaxjones@gmail.com>
2025-05-13 19:44:14 +1000

    remove javascrip parser

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const dateFormat = new Intl.DateTimeFormat("en-AU", {dateStyle: "full"});
-
-// atom parser adapted from: Coyier, C. (2020, February 11). How to Fetch and Parse RSS Feeds in JavaScript | CSS-Tricks. CSS-Tricks. https://css-tricks.com/how-to-fetch-and-parse-rss-feeds-in-javascript/
-fetch("feed.atom")
-  .then(response => response.text())
-  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => {
-    const entries = data.querySelectorAll("entry");
-    let HTML = ``;
-    for (const entry of entries) {
-      let title = entry.querySelector("title").innerHTML;
-      let published = entry.querySelector("published").innerHTML;
-      HTML = `
-        <article id="${title}">
-          <h2>#${title}</h2>
-          <p><time datetime="${published}">${dateFormat.format(new Date(published))}</time></p>
-          ${entry.querySelector("content div").innerHTML}
-        </article>
-      ` + HTML;
-    };
-    document.getElementById("feed").innerHTML = HTML;
-  });
-
 document.getElementById("atom-open").addEventListener("click", () => {
   document.getElementById("atom").show();
 });
