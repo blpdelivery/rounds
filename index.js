b38a17692e6ae3fc850af36431d5578409bbb692
Miro <miropaxjones@gmail.com>
2025-05-17 12:11:37 +1000

    add image preview

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 document.getElementById("atom-open").addEventListener("click", () => {
   document.getElementById("atom").show();
 });
+
+const imgPreview = document.getElementById("img-preview"),
+  imgPreviewClose = document.getElementById("img-preview-close"),
+  imgPreviewImg = document.getElementById("img-preview-img");
+
+imgPreview.addEventListener("click", () => imgPreview.close());
+imgPreviewClose.addEventListener("click", () => imgPreview.close());
+
+for (const mediaItem of document.querySelectorAll("img.media-item")) {
+  mediaItem.addEventListener("click", () => {
+    imgPreviewImg.src = mediaItem.src;
+    imgPreview.show();
+  });
+}
