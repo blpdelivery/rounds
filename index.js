f03688a483ae7a3abbefe81e9d6adb92dcadafac
Miro <miropaxjones@gmail.com>
2025-05-28 17:59:04 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,7 @@
 layout: js-minifier
 ---
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-    });
-  });
-});
-
-const atomURL = "https://rounds.bermaguilocalpost.org/feed.atom"
+const atomURL = "https://rounds.bermaguilocalpost.org/feed.atom";
 
 async function copyAtomURL() {
   try {
@@ -20,11 +10,12 @@ async function copyAtomURL() {
     alert("Feed URL copied to clipboard. Paste into your preferred feed reader.");
   } catch (err) {
     open(atomURL, "_blank");
-    console.error("copy to clipboard error: " + err);
+    console.error(`Copy to clipboard error (${err})`);
   }
 }
 
-document.getElementById("atom-open").addEventListener("click", copyAtomURL);
+document.getElementById("atom").addEventListener("click", copyAtomURL);
+
 
 const imgPreview = document.getElementById("img-preview"),
   imgPreviewClose = document.getElementById("img-preview-close"),
