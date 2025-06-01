a39d8fb0aa2a76788eddfdeebc49ba39df8c7024
Miro <miropaxjones@gmail.com>
2025-06-01 12:39:38 +1000

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ imgPreview.addEventListener("click", closeImgPreview);
 imgPreviewClose.addEventListener("click", closeImgPreview);
 
 for (const mediaItem of document.querySelectorAll("img.media-item")) {
+  mediaItem.addEventListener("error", () => mediaItem.src = "assets/ui/broken-img.svg");
   mediaItem.addEventListener("click", () => {
     imgPreviewImg.src = mediaItem.src;
     imgPreview.show();
