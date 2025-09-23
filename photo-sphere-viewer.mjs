6b5c4e9ea5caa3a05128c9f2ed2826ce78dbeefe
Miro <miropaxjones@gmail.com>
2025-09-23 13:13:45 +1000

    Create photo-sphere-viewer.mjs

---
+++ photo-sphere-viewer.mjs
@@ -0,0 +1,14 @@
+import { Viewer } from "@photo-sphere-viewer/core";
+import { CubemapAdapter } from "@photo-sphere-viewer/cubemap-adapter";
+
+for (const container of document.querySelectorAll(".photo-sphere.media-item")) {
+  const viewer = new Viewer({
+    container,
+    defaultZoomLvl: 0,
+    adapter: CubemapAdapter,
+    panorama: {
+      type: "stripe",
+      path: container.dataset.src
+    }
+  });
+}
