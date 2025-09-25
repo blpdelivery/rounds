1cbea300a90aa2fde5c91eff6fdc474423a0d1d8
Miro <miropaxjones@gmail.com>
2025-09-25 19:45:19 +1000

    Update photo-spheres.mjs

--- assets/js/photo-spheres.mjs
+++ assets/js/photo-spheres.mjs
@@ -2,6 +2,7 @@ import { Viewer } from "@photo-sphere-viewer/core";
 import { CubemapAdapter } from "@photo-sphere-viewer/cubemap-adapter";
 
 for (const container of document.querySelectorAll(".photo-sphere.media-item")) {
+  container.textContent = "";
   const viewer = new Viewer({
     container,
     defaultZoomLvl: 0,
